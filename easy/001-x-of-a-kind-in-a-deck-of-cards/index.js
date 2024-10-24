3157b3f7152fed78f2a9b6135cc9c33661b37368
Mauro Alvarez <contact@mauroalvarez.com>
2024-10-24 12:28:37 -0300

    Rename `x-of-a-kind-in-a-deck-of-cards` folder Add solution for `001-x-of-a-kind-in-a-deck-of-cards` problem

---
+++ easy/001-x-of-a-kind-in-a-deck-of-cards/index.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} deck
+ * @return {boolean}
+ */
+var hasGroupsSizeX = function (deck) {
+  if (deck.length === 1) {
+    return false;
+  }
+
+  let groups = {};
+  deck.map((card) => {
+    groups[card] = (groups[card] || 0) + 1;
+  });
+
+  function gcd(a, b) {
+    if (b === 0) {
+      return a;
+    }
+    return gcd(b, a % b);
+  }
+
+  let values = Object.values(groups);
+  let g = values[0];
+  for (let i = 0; i < values.length; i += 1) {
+    g = gcd(g, values[i]);
+    if (gcd(g, values[i]) === 1) {
+      return false;
+    }
+  }
+
+  return true;
+};
